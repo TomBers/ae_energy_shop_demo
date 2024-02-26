a2a21003487a2027b8198984018da7da4e64af7f
TomBers <tjcberman@gmail.com>
2024-02-26 12:15:49 +0000

    got intial file loading working

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from ollama import AsyncClient
 import chainlit as cl
 
+# Document Loaders
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=100)
+
 
 @cl.on_chat_start
 def start_chat():
@@ -10,11 +17,34 @@ def start_chat():
     )
 
 
+# TODO - Get File Loader tools working
+def process_file(file):
+    if file.mime == "text/plain":
+        with open(file.path, 'rb') as f:
+            file_content = f.read()
+            # Decode the binary content to a string
+            return file_content.decode('utf-8')
+    # elif file.mime == "application/pdf":
+    #     Loader = PyPDFLoader
+    else:
+        return None
+
+
 @cl.on_message
 async def main(message: cl.Message):
-
+    # Files are attached in the message object and can be accessed using message.elements
+    print(message.elements)
     message_history = cl.user_session.get("message_history")
     message_history.append({"role": "user", "content": message.content})
+
+    if message.elements:
+
+        for file in message.elements:
+            file_content = process_file(file)
+            print(file_content)
+            if file_content:
+                message_history.append(
+                    {"role": "user", "content": f"File Content: {file_content}"})
 
     msg = cl.Message(content="")
     await msg.send()
